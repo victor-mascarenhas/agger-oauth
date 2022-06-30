6fd5e7e508e70efcda4b3e22c343463c97af3b1d
Victor Mascarenhas Reis da Silva <78654482+victor-mascarenhas@users.noreply.github.com>
2022-06-30 09:46:33 -0300

    nav

--- models/nav.js
+++ models/nav.js
@@ -4,15 +4,11 @@ const { Schema } = mongoose;
 const NavSchema = new Schema({
   arrayPos: Number,
   type: { type: String, required: true },
-  htmlTag: { type: String, required: true},
-  key: { type: String, required: true},
-  href: { type: String, required: true},
-  text: { type: String, required: true},
-  style: { 
-    color: String,
-    fontSize: String,
-    fontFamily: String
-  },
+  htmlTag: { type: String, required: true },
+  key: { type: String, required: true },
+  href: { type: String, required: true },
+  text: { type: String, required: true },
+  style: {},
 });
 
 module.exports = mongoose.model("nav", NavSchema);
